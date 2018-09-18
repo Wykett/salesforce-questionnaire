6c8be80bdcac3294fba63d318cca37cf817deeb0
Wykett <bruno.chami@gmail.com>
2018-09-18 17:40:17 +0800

    Add code for questionnaire

---
+++ init_scratchorg.ps1
@@ -0,0 +1,3 @@
+param([string]$alias = (Get-Date).tostring("yyyyMMddhhmmss"))
+sfdx force:org:create -f config/project-scratch-def.json --setalias $alias --setdefaultusername --wait 2
+sfdx force:source:push
